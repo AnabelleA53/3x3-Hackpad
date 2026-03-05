4a0966fb09af7c8d4416d6f82986f124ae59dfb3
AnabelleA <anabelleahteck@gmail.com>
2026-03-05 00:04:12 -0500

    reorganized and added qmk firmware

---
+++ qmk/keymaps/AnabelleA53/keymap.c
@@ -0,0 +1,33 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* 
+     *      encoder
+     * ┌───┬───┬───┬
+     * │ 7 │ 8 │ 9 │
+     * ├───┼───┼───┼
+     * │ 4 │ 5 │ 6 │
+     * ├───┼───┼───┼
+     * │ 1 │ 2 │ 3 │
+     * └───┴───┴───┴
+     * 
+     */
+    [0] = LAYOUT(
+        /* Row 0: Encoder Press (Col 2)*/
+                          KC_MUTE,
+        /* Row 1: 3x3 Grid*/
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   KC_P3
+    )
+};
+
+/* Handles the knob turning for rotary encoder */
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+};
+#endif
---
+++ qmk/keymaps/default/keymap.c
@@ -0,0 +1,33 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │ 7 │ 8 │ 9 │ / │
+     * ├───┼───┼───┼───┤
+     * │ 4 │ 5 │ 6 │ * │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │ - │
+     * ├───┼───┼───┼───┤
+     * │ 0 │ . │Ent│ + │
+     * └───┴───┴───┴───┘
+     */
+    [0] = LAYOUT(
+        /* Row 0: Encoder Press (Col 2)*/
+                          KC_MUTE,
+        /* Row 1: 3x3 Grid*/
+        KC_P7,   KC_P8,   KC_P9,
+        KC_P4,   KC_P5,   KC_P6,
+        KC_P1,   KC_P2,   KC_P3
+    )
+};
+
+/* Handles the knob turning for rotary encoder */
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+};
+#endif
